8ab0c4bc7470bb5491562a452389bb92f7bbd23c
Dizzy B High <dizzybhigh@gmail.com>
2026-08-24 20:42:26 +0200

    Register Ctrl+Y redo hotkey in existing OBS hotkey module

--- src/workflow-hotkeys.c
+++ src/workflow-hotkeys.c
@@ -16,6 +16,7 @@ typedef struct shortcut_binding {
 
 static shortcut_binding_t bindings[MAX_SHORTCUT_BINDINGS];
 static size_t binding_count;
+static obs_hotkey_id redo_hotkey_id = OBS_INVALID_HOTKEY_ID;
 
 static void left_cb(void *data, obs_hotkey_id id, obs_hotkey_t *hotkey, bool pressed)
 {
@@ -44,6 +45,15 @@ static void shortcut_cb(void *data, obs_hotkey_id id, obs_hotkey_t *hotkey, bool
         workflow_shortcuts_accept(binding->workflow, binding->source_id, binding->target_id);
 }
 
+static void redo_cb(void *data, obs_hotkey_id id, obs_hotkey_t *hotkey, bool pressed)
+{
+    UNUSED_PARAMETER(data);
+    UNUSED_PARAMETER(id);
+    UNUSED_PARAMETER(hotkey);
+    if (pressed)
+        blog(LOG_INFO, "[Move Workflow] Ctrl+Y intercepted via OBS hotkey API.");
+}
+
 static void register_shortcut(workflow_t *workflow, const char *source_id,
                               const char *target_id)
 {
@@ -69,6 +79,13 @@ void workflow_hotkeys_register(void)
     obs_hotkey_register_frontend("obs_move_workflow.test_center", "Move Workflow: Test Center", center_cb, workflow);
     obs_hotkey_register_frontend("obs_move_workflow.test_right", "Move Workflow: Test Right", right_cb, workflow);
 
+    redo_hotkey_id = obs_hotkey_register_frontend(
+        "obs_move_workflow.redo", "Move Workflow: Redo", redo_cb, NULL);
+    struct obs_key_combination combo = {};
+    combo.modifiers = INTERACTION_CONTROL;
+    combo.key = OBS_KEY_Y;
+    obs_hotkey_register_binding(redo_hotkey_id, combo);
+
     binding_count = 0;
     for (size_t i = 0; i < workflow->node_count; ++i) {
         workflow_node_t *node = &workflow->nodes[i];
@@ -82,6 +99,10 @@ void workflow_hotkeys_unregister(void)
     for (size_t i = 0; i < binding_count; ++i) {
         if (bindings[i].id)
             obs_hotkey_unregister(bindings[i].id);
+    }
+    if (redo_hotkey_id != OBS_INVALID_HOTKEY_ID) {
+        obs_hotkey_unregister(redo_hotkey_id);
+        redo_hotkey_id = OBS_INVALID_HOTKEY_ID;
     }
     binding_count = 0;
     workflow_shortcuts_cancel();
