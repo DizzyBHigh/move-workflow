e95a3a1fbc3d4fd6e071ef0c07db50125d1be1b5
Dizzy B High <dizzybhigh@gmail.com>
2026-08-24 13:21:55 +0200

    Remove End relationship handling

--- src/workflow-scene.cpp
+++ src/workflow-scene.cpp
@@ -22,20 +22,6 @@ static void copy_text(char *destination, size_t capacity, const QString &value)
     destination[capacity - 1] = '\0';
 }
 
-static void remove_id(size_t &count, char ids[][WORKFLOW_MAX_NAME], const QString &id)
-{
-    const QByteArray wanted = id.toUtf8();
-    size_t write = 0;
-    for (size_t read = 0; read < count; ++read) {
-        if (std::strcmp(ids[read], wanted.constData()) != 0) {
-            if (write != read)
-                std::memcpy(ids[write], ids[read], WORKFLOW_MAX_NAME);
-            ++write;
-        }
-    }
-    count = write;
-}
-
 static bool add_node_id(size_t &count, char ids[][WORKFLOW_MAX_NAME], const QString &id)
 {
     const QByteArray bytes = id.toUtf8();
@@ -49,6 +35,20 @@ static bool add_node_id(size_t &count, char ids[][WORKFLOW_MAX_NAME], const QStr
     ids[count][WORKFLOW_MAX_NAME - 1] = '\0';
     ++count;
     return true;
+}
+
+static void remove_id(size_t &count, char ids[][WORKFLOW_MAX_NAME], const QString &id)
+{
+    const QByteArray wanted = id.toUtf8();
+    size_t write = 0;
+    for (size_t read = 0; read < count; ++read) {
+        if (std::strcmp(ids[read], wanted.constData()) != 0) {
+            if (write != read)
+                std::memcpy(ids[write], ids[read], WORKFLOW_MAX_NAME);
+            ++write;
+        }
+    }
+    count = write;
 }
 
 } // namespace
@@ -72,7 +72,6 @@ NodeItem *EditorScene::addNode(workflow_node_type_t type, const QString &name)
     node.workflow.start_delay.mode = WORKFLOW_OVERRIDE;
     node.workflow.end_delay.mode = WORKFLOW_OVERRIDE;
     node.workflow.simultaneous_actions_mode = WORKFLOW_OVERRIDE;
-    node.workflow.end_actions_mode = WORKFLOW_OVERRIDE;
     node.workflow.next_actions_mode = WORKFLOW_OVERRIDE;
     node.position = QPointF(80 + ((node.numeric_id - 1) % 4) * 310,
                             80 + ((node.numeric_id - 1) / 4) * 190);
@@ -86,17 +85,13 @@ NodeItem *EditorScene::addNode(workflow_node_type_t type, const QString &name)
 
 NodeItem *EditorScene::selectedNode() const
 {
-    for (QGraphicsItem *item : selectedItems()) {
+    for (QGraphicsItem *item : selectedItems())
         if (auto *node = dynamic_cast<NodeItem *>(item))
             return node;
-    }
     return nullptr;
 }
 
-QList<NodeItem *> EditorScene::nodes() const
-{
-    return nodes_;
-}
+QList<NodeItem *> EditorScene::nodes() const { return nodes_; }
 
 void EditorScene::deleteNode(NodeItem *node)
 {
@@ -104,10 +99,10 @@ void EditorScene::deleteNode(NodeItem *node)
         return;
     const QString id = node->id();
     for (NodeItem *candidate : nodes_) {
-        workflow_node_t *wf = candidate->workflowNode();
-        remove_id(wf->end_node_count, wf->end_node_ids, id);
+        auto *wf = candidate->workflowNode();
         remove_id(wf->simultaneous_node_count, wf->simultaneous_node_ids, id);
         remove_id(wf->next_node_count, wf->next_node_ids, id);
+        remove_id(wf->shortcut_node_count, wf->shortcut_node_ids, id);
     }
     nodes_.removeAll(node);
     removeItem(node);
@@ -131,11 +126,11 @@ void EditorScene::rebuildConnections()
     connections_.clear();
 
     for (NodeItem *from : nodes_) {
-        const workflow_node_t *wf = from->workflowNode();
-        addRelationshipLines(from, wf->end_node_count, wf->end_node_ids, "End Action");
-        addRelationshipLines(from, wf->simultaneous_node_count, wf->simultaneous_node_ids,
-                             "Simultaneous");
+        const auto *wf = from->workflowNode();
+        addRelationshipLines(from, wf->simultaneous_node_count,
+                             wf->simultaneous_node_ids, "Simultaneous");
         addRelationshipLines(from, wf->next_node_count, wf->next_node_ids, "Next Action");
+        addRelationshipLines(from, wf->shortcut_node_count, wf->shortcut_node_ids, "Shortcut");
     }
     updateConnections();
 }
@@ -227,8 +222,7 @@ void EditorScene::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
 void EditorScene::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
 {
     if (draggingConnection_ && event->button() == Qt::LeftButton) {
-        const QPointF releasePos = event->scenePos();
-        finishConnectionDrag(releasePos);
+        finishConnectionDrag(event->scenePos());
         event->accept();
         return;
     }
@@ -265,11 +259,9 @@ NodeItem *EditorScene::findNodeById(const char *id) const
 {
     if (!id)
         return nullptr;
-
-    for (NodeItem *node : nodes_) {
+    for (NodeItem *node : nodes_)
         if (node && node->id() == QString::fromUtf8(id))
             return node;
-    }
     return nullptr;
 }
 
@@ -277,7 +269,6 @@ void EditorScene::finishConnectionDrag(const QPointF &scenePos)
 {
     NodeItem *source = dragSource_;
     NodeItem *target = nodeAt(scenePos);
-
     if (dragPreview_) {
         removeItem(dragPreview_);
         delete dragPreview_;
@@ -285,7 +276,6 @@ void EditorScene::finishConnectionDrag(const QPointF &scenePos)
     dragPreview_ = nullptr;
     dragSource_ = nullptr;
     draggingConnection_ = false;
-
     if (!source || !target || source == target)
         return;
 
@@ -296,13 +286,15 @@ void EditorScene::finishConnectionDrag(const QPointF &scenePos)
                            QMessageBox::NoButton, nullptr);
         QPushButton *simultaneous = dialog.addButton("Simultaneous", QMessageBox::AcceptRole);
         QPushButton *next = dialog.addButton("Next", QMessageBox::AcceptRole);
+        QPushButton *shortcut = dialog.addButton("Shortcut", QMessageBox::AcceptRole);
         QPushButton *cancel = dialog.addButton("Cancel", QMessageBox::RejectRole);
         dialog.exec();
-
         if (dialog.clickedButton() == simultaneous)
             connectActionToAction(source, target, "Simultaneous");
         else if (dialog.clickedButton() == next)
             connectActionToAction(source, target, "Next");
+        else if (dialog.clickedButton() == shortcut)
+            connectActionToAction(source, target, "Shortcut");
         else if (dialog.clickedButton() == cancel)
             return;
     } else if (source->workflowNode()->type == WORKFLOW_NODE_TRIGGER &&
@@ -314,7 +306,6 @@ void EditorScene::finishConnectionDrag(const QPointF &scenePos)
     } else {
         return;
     }
-
     source->refreshDisplay();
     target->refreshDisplay();
     rebuildConnections();
@@ -324,19 +315,19 @@ void EditorScene::connectTriggerToAction(NodeItem *trigger, NodeItem *action)
 {
     if (!trigger || !action)
         return;
-
     add_node_id(trigger->workflowNode()->simultaneous_node_count,
-                trigger->workflowNode()->simultaneous_node_ids,
-                action->id());
+                trigger->workflowNode()->simultaneous_node_ids, action->id());
 }
 
 void EditorScene::connectActionToAction(NodeItem *source, NodeItem *target, const QString &type)
 {
-    workflow_node_t *wf = source->workflowNode();
+    auto *wf = source->workflowNode();
     if (type == "Simultaneous")
         add_node_id(wf->simultaneous_node_count, wf->simultaneous_node_ids, target->id());
-    else
+    else if (type == "Next")
         add_node_id(wf->next_node_count, wf->next_node_ids, target->id());
+    else
+        add_node_id(wf->shortcut_node_count, wf->shortcut_node_ids, target->id());
 }
 
 void EditorScene::addRelationshipLines(NodeItem *from, size_t count,
@@ -347,63 +338,29 @@ void EditorScene::addRelationshipLines(NodeItem *from, size_t count,
         NodeItem *to = findNodeById(ids[i]);
         if (!to || to == from)
             continue;
-        if (from->workflowNode()->type == WORKFLOW_NODE_ACTION &&
-            to->workflowNode()->type == WORKFLOW_NODE_TRIGGER)
-            continue;
         auto *line = new QGraphicsPathItem;
         if (type == "Simultaneous")
             line->setPen(QPen(QColor(90, 190, 120), 2));
         else if (type == "Next Action")
             line->setPen(QPen(QColor(230, 170, 70), 2));
         else
-            line->setPen(QPen(QColor(70, 160, 230), 2));
+            line->setPen(QPen(QColor(180, 120, 220), 2));
         line->setZValue(-1);
         addItem(line);
         connections_.push_back({from, to, line, type});
-        updateConnection(line, from, to);
     }
 }
 
 void EditorScene::updateConnection(QGraphicsPathItem *line, NodeItem *from, NodeItem *to)
 {
-    const QRectF aRect = from->sceneBoundingRect();
-    const QRectF bRect = to->sceneBoundingRect();
-    const QPointF fromCenter = aRect.center();
-    const QPointF toCenter = bRect.center();
-    const qreal dx = toCenter.x() - fromCenter.x();
-    const qreal dy = toCenter.y() - fromCenter.y();
-    const qreal distance = qMax<qreal>(40.0, std::sqrt(dx * dx + dy * dy) * 0.35);
-
-    QPointF a;
-    QPointF b;
-    QPointF controlA;
-    QPointF controlB;
-
-    if (qAbs(dx) >= qAbs(dy)) {
-        if (dx >= 0.0) {
-            a = QPointF(aRect.right(), aRect.center().y());
-            b = QPointF(bRect.left(), bRect.center().y());
-            controlA = a + QPointF(distance, 0);
-            controlB = b - QPointF(distance, 0);
-        } else {
-            a = QPointF(aRect.left(), aRect.center().y());
-            b = QPointF(bRect.right(), bRect.center().y());
-            controlA = a - QPointF(distance, 0);
-            controlB = b + QPointF(distance, 0);
-        }
-    } else if (dy >= 0.0) {
-        a = QPointF(aRect.center().x(), aRect.bottom());
-        b = QPointF(bRect.center().x(), bRect.top());
-        controlA = a + QPointF(0, distance);
-        controlB = b - QPointF(0, distance);
-    } else {
-        a = QPointF(aRect.center().x(), aRect.top());
-        b = QPointF(bRect.center().x(), bRect.bottom());
-        controlA = a - QPointF(0, distance);
-        controlB = b + QPointF(0, distance);
-    }
-
-    QPainterPath path(a);
-    path.cubicTo(controlA, controlB, b);
+    if (!line || !from || !to)
+        return;
+    const QPointF start = from->sceneBoundingRect().center();
+    const QPointF end = to->sceneBoundingRect().center();
+    const qreal dx = end.x() - start.x();
+    const qreal dy = end.y() - start.y();
+    QPainterPath path(start);
+    path.cubicTo(start + QPointF(dx * 0.35, dy * 0.05),
+                 end - QPointF(dx * 0.35, dy * 0.05), end);
     line->setPath(path);
 }
