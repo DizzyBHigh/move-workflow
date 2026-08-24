ddad1025be1cfd5a093d0d7a563144a5077f8324
Dizzy B High <dizzybhigh@gmail.com>
2026-08-24 14:11:54 +0200

    Extract existing OBS test hotkeys

--- src/workflow-hotkeys.c
+++ src/workflow-hotkeys.c
@@ -1,13 +1,35 @@
 #include "workflow-hotkeys.h"
+#include "workflow-runtime.h"
 
+#include <obs-frontend-api.h>
 #include <obs-module.h>
+
+static void left_cb(void *data, obs_hotkey_id id, obs_hotkey_t *hotkey, bool pressed)
+{
+    UNUSED_PARAMETER(id); UNUSED_PARAMETER(hotkey);
+    if (pressed) workflow_runtime_execute_node_by_id(data, "move-left");
+}
+
+static void center_cb(void *data, obs_hotkey_id id, obs_hotkey_t *hotkey, bool pressed)
+{
+    UNUSED_PARAMETER(id); UNUSED_PARAMETER(hotkey);
+    if (pressed) workflow_runtime_execute_node_by_id(data, "move-center");
+}
+
+static void right_cb(void *data, obs_hotkey_id id, obs_hotkey_t *hotkey, bool pressed)
+{
+    UNUSED_PARAMETER(id); UNUSED_PARAMETER(hotkey);
+    if (pressed) workflow_runtime_execute_node_by_id(data, "move-right");
+}
 
 void workflow_hotkeys_register(void)
 {
-    /* Registration is completed when Shortcut runtime state is available. */
+    workflow_t *workflow = workflow_runtime_test_workflow();
+    obs_hotkey_register_frontend("obs_move_workflow.test_left", "Move Workflow: Test Left", left_cb, workflow);
+    obs_hotkey_register_frontend("obs_move_workflow.test_center", "Move Workflow: Test Center", center_cb, workflow);
+    obs_hotkey_register_frontend("obs_move_workflow.test_right", "Move Workflow: Test Right", right_cb, workflow);
 }
 
 void workflow_hotkeys_unregister(void)
 {
-    /* No global hook is used; OBS owns all keyboard handling. */
 }
