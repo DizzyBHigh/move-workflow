3dc28080d8a866d139f576fee9561a0658116cb7
Dizzy B High <dizzybhigh@gmail.com>
2026-08-24 20:46:04 +0200

    Expose workflow redo hotkey callback bridge

--- src/workflow-hotkeys.h
+++ src/workflow-hotkeys.h
@@ -4,8 +4,11 @@
 extern "C" {
 #endif
 
+typedef void (*workflow_redo_callback_t)(void);
+
 void workflow_hotkeys_register(void);
 void workflow_hotkeys_unregister(void);
+void workflow_hotkeys_set_redo_callback(workflow_redo_callback_t callback);
 
 #ifdef __cplusplus
 }
