146e3388e68648f5ed5a5f736fdaeea52e5236ff
Dizzy B High <dizzybhigh@gmail.com>
2026-08-24 10:52:32 +0200

    Replace node handles with full-width drag bar

--- src/workflow-node.h
+++ src/workflow-node.h
@@ -31,15 +31,11 @@ protected:
 private:
     static constexpr qreal nodeWidth = 300.0;
     static constexpr qreal minimumHeight = 142.0;
-    static constexpr qreal handleRadius = 10.0;
-    static constexpr qreal handleHitRadius = 18.0;
+    static constexpr qreal dragBarHeight = 10.0;
+    static constexpr qreal dragBarHitHeight = 22.0;
 
     void updateGeometryForText();
     void refreshStyle();
-    QPointF inputHandlePos() const;
-    QPointF outputHandlePos() const;
-    QPointF topHandlePos() const;
-    QPointF bottomHandlePos() const;
 
     EditorNode node_;
     QGraphicsTextItem *title_ = nullptr;
