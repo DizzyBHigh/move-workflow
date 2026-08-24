cbc9985dadc83a9af3406f0d0b991e0c6cb0cdb5
Dizzy B High <dizzybhigh@gmail.com>
2026-08-24 14:54:12 +0200

    Add workflow manager interface

---
+++ src/workflow-manager.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "workflow-model.h"
+
+#define WORKFLOW_MANAGER_MAX_WORKFLOWS 32
+
+typedef struct workflow_manager {
+    workflow_t workflows[WORKFLOW_MANAGER_MAX_WORKFLOWS];
+    size_t workflow_count;
+    char selected_workflow_id[WORKFLOW_MAX_NAME];
+} workflow_manager_t;
+
+void workflow_manager_init(workflow_manager_t *manager);
+workflow_t *workflow_manager_create(workflow_manager_t *manager,
+                                    const char *id, const char *name);
+bool workflow_manager_remove(workflow_manager_t *manager, const char *id);
+workflow_t *workflow_manager_find(workflow_manager_t *manager, const char *id);
+const workflow_t *workflow_manager_find_const(const workflow_manager_t *manager,
+                                              const char *id);
+bool workflow_manager_set_enabled(workflow_manager_t *manager,
+                                   const char *id, bool enabled);
+bool workflow_manager_set_selected(workflow_manager_t *manager, const char *id);
+workflow_t *workflow_manager_selected(workflow_manager_t *manager);
+const workflow_t *workflow_manager_selected_const(const workflow_manager_t *manager);
