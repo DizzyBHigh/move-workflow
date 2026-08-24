36a5fe4ea22744bf6d87189ac9bdaaf9fe0bc84f
Dizzy B High <dizzybhigh@gmail.com>
2026-08-24 15:25:53 +0200

    Add safe workflow create and duplicate workspace operations

--- src/workflow-workspace.h
+++ src/workflow-workspace.h
@@ -16,6 +16,8 @@ typedef struct workflow_workspace {
 void workflow_workspace_init(workflow_workspace_t *workspace, EditorScene *scene);
 void workflow_workspace_sync_scene(workflow_workspace_t *workspace);
 bool workflow_workspace_select(workflow_workspace_t *workspace, const char *id);
+bool workflow_workspace_create(workflow_workspace_t *workspace, const char *name);
+bool workflow_workspace_duplicate(workflow_workspace_t *workspace, const char *name);
 workflow_manager_t *workflow_workspace_manager(workflow_workspace_t *workspace);
 
 #ifdef __cplusplus
