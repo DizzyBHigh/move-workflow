76d26b684003593504fd844c4af7e41241cc9847
Dizzy B High <dizzybhigh@gmail.com>
2026-08-24 10:26:24 +0200

    Add scene connection drag state

--- src/workflow-scene.h
+++ src/workflow-scene.h
@@ -27,6 +27,9 @@ signals:
     void nodeDoubleClicked(NodeItem *node);
 
 protected:
+    void mousePressEvent(QGraphicsSceneMouseEvent *event) override;
+    void mouseMoveEvent(QGraphicsSceneMouseEvent *event) override;
+    void mouseReleaseEvent(QGraphicsSceneMouseEvent *event) override;
     void mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event) override;
 
 private:
@@ -38,12 +41,20 @@ private:
     };
 
     NodeItem *findNodeById(const char *id) const;
+    NodeItem *nodeAt(const QPointF &scenePos) const;
 
     void addRelationshipLines(
         NodeItem *from,
         size_t count,
         const char ids[][WORKFLOW_MAX_NAME],
         const QString &type);
+
+    bool addNodeId(size_t &count, char ids[][WORKFLOW_MAX_NAME], const QString &id);
+    bool hasNodeId(size_t count, const char ids[][WORKFLOW_MAX_NAME], const QString &id) const;
+    void connectNodes(NodeItem *source, NodeItem *target);
+    void connectTriggerToAction(NodeItem *trigger, NodeItem *action);
+    void connectActionToAction(NodeItem *source, NodeItem *target, const QString &type);
+    void finishConnectionDrag(const QPointF &scenePos);
 
     static void updateConnection(
         QGraphicsPathItem *line,
@@ -53,4 +64,8 @@ private:
     int nextId_ = 0;
     QVector<NodeItem *> nodes_;
     QVector<Connection> connections_;
+
+    NodeItem *dragSource_ = nullptr;
+    QGraphicsPathItem *dragPreview_ = nullptr;
+    bool draggingConnection_ = false;
 };
