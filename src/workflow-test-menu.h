56728aea4e17269609c929346561076d73fd96b1
Dizzy B High <dizzybhigh@gmail.com>
2026-08-25 14:27:55 +0200

    Expose workflow engine test menu context

--- src/workflow-test-menu.h
+++ src/workflow-test-menu.h
@@ -1,12 +1,14 @@
 #pragma once
 
+#include "workflow-engine.h"
 #include "workflow-model.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void workflow_test_menu_register(workflow_t *workflow);
+void workflow_test_menu_register(workflow_t *workflow,
+                                 workflow_engine_t *engine);
 
 #ifdef __cplusplus
 }
