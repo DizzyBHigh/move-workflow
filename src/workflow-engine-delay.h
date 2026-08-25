9a354c4c6363e88fd247144aadfdac142663e910
Dizzy B High <dizzybhigh@gmail.com>
2026-08-25 14:12:40 +0200

    Add execution delay API

---
+++ src/workflow-engine-delay.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void (*workflow_engine_delay_callback_t)(void *data);
+
+bool workflow_engine_delay_start(uint64_t delay_ms,
+                                 workflow_engine_delay_callback_t callback,
+                                 void *data);
+
+#ifdef __cplusplus
+}
+#endif
