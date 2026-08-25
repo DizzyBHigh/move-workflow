b6f98da9f87a5b5698bec6abd44550f35ef5c032
Dizzy B High <dizzybhigh@gmail.com>
2026-08-25 14:13:32 +0200

    Clarify runner internal API names

--- src/workflow-engine-runner.h
+++ src/workflow-engine-runner.h
@@ -6,8 +6,8 @@
 extern "C" {
 #endif
 
-bool workflow_engine_run_entries(workflow_engine_state_t *state);
-bool workflow_engine_run_node(workflow_engine_state_t *state, const char *node_id);
+bool workflow_engine_runner_run_entries(workflow_engine_state_t *state);
+bool workflow_engine_runner_run_node(workflow_engine_state_t *state, const char *node_id);
 
 #ifdef __cplusplus
 }
