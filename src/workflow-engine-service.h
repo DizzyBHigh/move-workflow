eacb68af6d55abeb2e2d43e12af2bebc450178a5
Dizzy B High <dizzybhigh@gmail.com>
2026-08-25 14:42:28 +0200

    Resolve test workflow through persistence manager

---
+++ src/workflow-engine-service.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct workflow_engine;
+void workflow_engine_service_set(struct workflow_engine *engine);
+bool workflow_engine_service_test_node(const char *workflow_id, const char *node_id);
+
+#ifdef __cplusplus
+}
+#endif
