ac1eba81ba801ec962fb724f73fa29a4576155c5
Dizzy B High <dizzybhigh@gmail.com>
2026-08-25 14:15:43 +0200

    Clarify trigger dispatch API

--- src/workflow-engine-trigger.h
+++ src/workflow-engine-trigger.h
@@ -10,9 +10,9 @@ extern "C" {
 workflow_node_t *workflow_engine_find_trigger(workflow_t *workflow,
                                                workflow_trigger_type_t type,
                                                const char *value);
-bool workflow_engine_trigger(workflow_engine_state_t *state,
-                             workflow_trigger_type_t type,
-                             const char *value);
+bool workflow_engine_trigger_dispatch(workflow_engine_state_t *state,
+                                      workflow_trigger_type_t type,
+                                      const char *value);
 
 #ifdef __cplusplus
 }
