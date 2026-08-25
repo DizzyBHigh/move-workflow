3fbe032df3d16c2af75bf5ae389969649b072f3f
Dizzy B High <dizzybhigh@gmail.com>
2026-08-25 14:09:46 +0200

    Add execution engine public API

---
+++ src/workflow-engine.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "workflow-model.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct workflow_engine workflow_engine_t;
+
+workflow_engine_t *workflow_engine_create(void);
+void workflow_engine_destroy(workflow_engine_t *engine);
+
+bool workflow_engine_start(workflow_engine_t *engine, workflow_t *workflow);
+void workflow_engine_stop(workflow_engine_t *engine);
+bool workflow_engine_is_running(const workflow_engine_t *engine);
+
+#ifdef __cplusplus
+}
+#endif
