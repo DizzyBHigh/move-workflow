38b84b5e93cdb98cc793522bcd258909c024593a
Dizzy B High <dizzybhigh@gmail.com>
2026-08-25 14:41:49 +0200

    Expose direct execution node entry for testing

--- src/workflow-engine.h
+++ src/workflow-engine.h
@@ -15,6 +15,7 @@ bool workflow_engine_start(workflow_engine_t *engine, workflow_t *workflow);
 void workflow_engine_stop(workflow_engine_t *engine);
 bool workflow_engine_is_running(const workflow_engine_t *engine);
 bool workflow_engine_run_entries(workflow_engine_t *engine);
+bool workflow_engine_run_node(workflow_engine_t *engine, const char *node_id);
 bool workflow_engine_trigger(workflow_engine_t *engine,
                              workflow_trigger_type_t type,
                              const char *value);
