74b57a979b696cabbb2d30a9e1d4f20bb644c80a
Dizzy B High <dizzybhigh@gmail.com>
2026-08-25 16:01:42 +0200

    Support dispatching triggers across active workflow runs

--- src/workflow-engine-runs.h
+++ src/workflow-engine-runs.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "workflow-engine-state.h"
+#include "workflow-trigger-types.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -17,6 +18,9 @@ workflow_engine_state_t *workflow_engine_run_state(workflow_engine_run_t *run);
 workflow_engine_run_t *workflow_engine_runs_current(workflow_engine_runs_t *runs);
 void workflow_engine_runs_stop_all(workflow_engine_runs_t *runs);
 bool workflow_engine_runs_any_active(const workflow_engine_runs_t *runs);
+bool workflow_engine_runs_trigger(workflow_engine_runs_t *runs,
+                                   workflow_trigger_type_t type,
+                                   const char *value);
 
 #ifdef __cplusplus
 }
