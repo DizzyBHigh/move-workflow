74bc6ef172fdff4726afb2bd36688e45b66d6aaf
Dizzy B High <dizzybhigh@gmail.com>
2026-08-25 14:15:20 +0200

    Expose engine trigger dispatch

--- src/workflow-engine.h
+++ src/workflow-engine.h
@@ -15,6 +15,9 @@ bool workflow_engine_start(workflow_engine_t *engine, workflow_t *workflow);
 void workflow_engine_stop(workflow_engine_t *engine);
 bool workflow_engine_is_running(const workflow_engine_t *engine);
 bool workflow_engine_run_entries(workflow_engine_t *engine);
+bool workflow_engine_trigger(workflow_engine_t *engine,
+                             workflow_trigger_type_t type,
+                             const char *value);
 
 #ifdef __cplusplus
 }
