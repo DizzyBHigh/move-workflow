afe8305f3cbefa91252a6907aa76d1762c6504ef
Dizzy B High <dizzybhigh@gmail.com>
2026-08-25 21:46:38 +0200

    Preserve override values when timing defaults are enabled

--- src/workflow-node-settings.cpp
+++ src/workflow-node-settings.cpp
@@ -94,11 +94,14 @@ bool NodeSettingsDialog::apply()
     }
 
     wf->start_delay.mode = startDelayDefault_->isChecked() ? WORKFLOW_USE_EXISTING : WORKFLOW_OVERRIDE;
-    wf->start_delay.delay_ms = (uint64_t)startDelayMs_->value();
+    wf->start_delay.delay_ms = startDelayDefault_->isChecked()
+                                   ? startDelayOverrideMs_ : (uint64_t)startDelayMs_->value();
     wf->duration.mode = durationDefault_->isChecked() ? WORKFLOW_USE_EXISTING : WORKFLOW_OVERRIDE;
-    wf->duration.duration_ms = (uint64_t)durationMs_->value();
+    wf->duration.duration_ms = durationDefault_->isChecked()
+                                   ? durationOverrideMs_ : (uint64_t)durationMs_->value();
     wf->end_delay.mode = endDelayDefault_->isChecked() ? WORKFLOW_USE_EXISTING : WORKFLOW_OVERRIDE;
-    wf->end_delay.delay_ms = (uint64_t)endDelayMs_->value();
+    wf->end_delay.delay_ms = endDelayDefault_->isChecked()
+                                 ? endDelayOverrideMs_ : (uint64_t)endDelayMs_->value();
     simultaneous_->apply(wf->simultaneous_node_count, wf->simultaneous_node_ids);
     nextActions_->apply(wf->next_node_count, wf->next_node_ids);
     shortcutActions_->apply(wf->shortcut_node_count, wf->shortcut_node_ids);
