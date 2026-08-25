1f98549b3382d9a75d5672ccda7bd38c98587d30
Dizzy B High <dizzybhigh@gmail.com>
2026-08-25 19:31:00 +0200

    execution-engine: add action duration runtime API

--- src/workflow-action-runtime.h
+++ src/workflow-action-runtime.h
@@ -19,33 +19,28 @@ typedef enum workflow_action_runtime_state {
 typedef struct workflow_action_runtime workflow_action_runtime_t;
 
 workflow_action_runtime_t *workflow_action_runtime_create(
-    workflow_t *workflow,
-    workflow_node_t *node,
-    uint64_t generation);
-
+    workflow_t *workflow, workflow_node_t *node, uint64_t generation);
 void workflow_action_runtime_destroy(workflow_action_runtime_t *runtime);
 
 workflow_action_runtime_state_t workflow_action_runtime_state(
     const workflow_action_runtime_t *runtime);
+void workflow_action_runtime_begin_execution(workflow_action_runtime_t *runtime);
+void workflow_action_runtime_complete_duration(
+    workflow_action_runtime_t *runtime);
+void workflow_action_runtime_complete(workflow_action_runtime_t *runtime);
 
 workflow_t *workflow_action_runtime_workflow(
     const workflow_action_runtime_t *runtime);
-
 workflow_node_t *workflow_action_runtime_node(
     const workflow_action_runtime_t *runtime);
-
 uint64_t workflow_action_runtime_generation(
     const workflow_action_runtime_t *runtime);
-
 uint64_t workflow_action_runtime_duration_ms(
     const workflow_action_runtime_t *runtime);
-
 uint64_t workflow_action_runtime_start_delay_ms(
     const workflow_action_runtime_t *runtime);
-
 uint64_t workflow_action_runtime_end_delay_ms(
     const workflow_action_runtime_t *runtime);
-
 bool workflow_action_runtime_has_duration(
     const workflow_action_runtime_t *runtime);
 
