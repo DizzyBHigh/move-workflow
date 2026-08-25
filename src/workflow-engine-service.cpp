2edf994519d17e0507c2669e3d8aee00132a015e
Dizzy B High <dizzybhigh@gmail.com>
2026-08-25 14:42:47 +0200

    Run trigger-node tests against selected workflow

--- src/workflow-engine-service.cpp
+++ src/workflow-engine-service.cpp
@@ -1,7 +1,8 @@
 #include "workflow-engine-service.h"
 
-#include "workflow-engine.h"
 #include "workflow-debug.h"
+#include "workflow-engine.h"
+#include "workflow-persistence.h"
 
 static workflow_engine_t *service_engine;
 
@@ -10,11 +11,15 @@ void workflow_engine_service_set(workflow_engine_t *engine)
     service_engine = engine;
 }
 
-bool workflow_engine_service_test_node(workflow_t *workflow, const char *node_id)
+bool workflow_engine_service_test_node(const char *workflow_id, const char *node_id)
 {
-    if (!service_engine || !workflow || !node_id || !*node_id)
+    if (!service_engine || !workflow_id || !node_id || !*node_id)
         return false;
-    if (!workflow_engine_start(service_engine, workflow))
+    workflow_manager_t *manager = workflow_persistence_manager();
+    if (!manager)
+        return false;
+    workflow_t *workflow = workflow_manager_find(manager, workflow_id);
+    if (!workflow || !workflow_engine_start(service_engine, workflow))
         return false;
     workflow_debug_log("TEST trigger node: %s", node_id);
     return workflow_engine_run_node(service_engine, node_id);
