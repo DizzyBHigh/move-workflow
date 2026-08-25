d6ae34d18caccbbfed92bc71192d0e3b89f436a3
Dizzy B High <dizzybhigh@gmail.com>
2026-08-25 22:31:18 +0200

    Wire workflow nodes to temporary filter instances

--- src/workflow-filter-instance.h
+++ src/workflow-filter-instance.h
@@ -1,8 +1,7 @@
 #pragma once
 
 #include <obs.h>
-
-struct workflow_node;
+#include "workflow-model.h"
 
 struct workflow_filter_instance {
 	obs_source_t *original;
@@ -12,8 +11,10 @@ struct workflow_filter_instance {
 
 workflow_filter_instance *workflow_filter_instance_create(obs_source_t *original,
 										obs_source_t *parent,
-										const workflow_node *node);
+										const workflow_node_t *node);
 
 bool workflow_filter_instance_execute(workflow_filter_instance *instance);
-
 void workflow_filter_instance_destroy(workflow_filter_instance *instance);
+
+bool workflow_filter_instance_execute_node(workflow_t *workflow,
+										workflow_node_t *node);
