90625a88a6eb411a4723d2cb7eda5651b85f9254
Dizzy B High <dizzybhigh@gmail.com>
2026-08-25 14:12:00 +0200

    Add execution engine state API

---
+++ src/workflow-engine-state.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "workflow-model.h"
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct workflow_engine_state {
+    workflow_t *workflow;
+    char current_node_id[WORKFLOW_MAX_NAME];
+    bool running;
+    bool stopping;
+    unsigned long generation;
+    size_t pending_branches;
+} workflow_engine_state_t;
+
+void workflow_engine_state_reset(workflow_engine_state_t *state);
+void workflow_engine_state_begin(workflow_engine_state_t *state, workflow_t *workflow);
+void workflow_engine_state_stop(workflow_engine_state_t *state);
+bool workflow_engine_state_is_active(const workflow_engine_state_t *state);
+
+#ifdef __cplusplus
+}
+#endif
