5c8c15c7d5f52fed45575b21ecd68f689378457a
Dizzy B High <dizzybhigh@gmail.com>
2026-08-26 16:21:32 +0200

    Add workflow editor sidebar interface

---
+++ src/workflow-editor-sidebar.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "workflow-manager.h"
+#include "workflow-model.h"
+
+#include <functional>
+
+class QWidget;
+
+struct workflow_editor_sidebar_callbacks {
+    std::function<void(const char *)> select_workflow;
+    std::function<bool(const char *)> create_workflow;
+    std::function<bool(const char *)> duplicate_workflow;
+    std::function<bool()> delete_workflow;
+    std::function<void()> rename_workflow;
+    std::function<void(bool)> set_workflow_enabled;
+    std::function<void(const char *)> add_node;
+};
+
+QWidget *create_workflow_editor_sidebar(
+    workflow_manager_t *manager,
+    QWidget *parent,
+    workflow_editor_sidebar_callbacks callbacks);
+
+void workflow_editor_sidebar_refresh(QWidget *sidebar);
