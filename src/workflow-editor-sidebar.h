b07f4180b6eb5d5e62ef19ee60147891c70ea714
Dizzy B High <dizzybhigh@gmail.com>
2026-08-26 17:07:18 +0200

    Add workflow node list selection

--- src/workflow-editor-sidebar.h
+++ src/workflow-editor-sidebar.h
@@ -1,12 +1,15 @@
 #pragma once
 
 #include <functional>
+#include <QList>
 
+class NodeItem;
 class QWidget;
 
 struct workflow_editor_sidebar_callbacks {
     std::function<void()> add_trigger;
     std::function<void(const char *)> add_node;
+    std::function<void(const char *)> select_node;
     std::function<void()> edit_node;
     std::function<void()> copy_node;
     std::function<void()> paste_node;
@@ -16,6 +19,5 @@ struct workflow_editor_sidebar_callbacks {
 
 QWidget *create_workflow_editor_sidebar(QWidget *parent,
                                         workflow_editor_sidebar_callbacks callbacks);
-void workflow_editor_sidebar_set_selection_state(QWidget *sidebar,
-                                                  bool has_selection,
-                                                  bool can_paste);
+void workflow_editor_sidebar_set_selection_state(QWidget *sidebar, bool has_selection, bool can_paste);
+void workflow_editor_sidebar_set_workflow_nodes(QWidget *sidebar, const QList<NodeItem *> &nodes, NodeItem *selected);
