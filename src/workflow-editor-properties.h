5c1729b73f905c4a28cdd0e1bb9ad5de0243a4e5
Dizzy B High <dizzybhigh@gmail.com>
2026-08-26 16:24:00 +0200

    Allow workflow properties edit callback

--- src/workflow-editor-properties.h
+++ src/workflow-editor-properties.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <functional>
+
 class NodeItem;
 class QWidget;
 
-QWidget *create_workflow_editor_properties(QWidget *parent, void (*edit_node)(NodeItem *));
+QWidget *create_workflow_editor_properties(QWidget *parent, std::function<void(NodeItem *)> edit_node);
 void workflow_editor_properties_set_node(QWidget *properties, NodeItem *node);
