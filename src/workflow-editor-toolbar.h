ae9649b99a0768f49cb95bda008ad74709e6502d
Dizzy B High <dizzybhigh@gmail.com>
2026-08-26 16:58:13 +0200

    Reorganize workflow controls into top toolbar

--- src/workflow-editor-toolbar.h
+++ src/workflow-editor-toolbar.h
@@ -1,17 +1,19 @@
 #pragma once
 
+#include "workflow-manager.h"
 #include <functional>
 
 class QWidget;
 
 struct workflow_editor_toolbar_callbacks {
-    std::function<void()> add;
-    std::function<void()> edit;
-    std::function<void()> copy;
-    std::function<void()> paste;
-    std::function<void()> duplicate;
-    std::function<void()> remove;
-    std::function<void()> test;
+    std::function<void(const char *)> select_workflow;
+    std::function<bool(const char *)> create_workflow;
+    std::function<bool(const char *)> duplicate_workflow;
+    std::function<bool()> delete_workflow;
+    std::function<void()> rename_workflow;
+    std::function<void(bool)> set_workflow_enabled;
+    std::function<void()> import_workflow;
+    std::function<void()> export_workflow;
     std::function<void()> zoom_out;
     std::function<void()> zoom_reset;
     std::function<void()> zoom_in;
@@ -19,9 +21,7 @@ struct workflow_editor_toolbar_callbacks {
     std::function<void()> close;
 };
 
-QWidget *create_workflow_editor_toolbar(
-    QWidget *parent,
-    workflow_editor_toolbar_callbacks callbacks);
-
-void workflow_editor_toolbar_set_selection_state(
-    QWidget *toolbar, bool has_selection, bool can_paste);
+QWidget *create_workflow_editor_toolbar(QWidget *parent,
+                                        workflow_manager_t *manager,
+                                        workflow_editor_toolbar_callbacks callbacks);
+void workflow_editor_toolbar_refresh(QWidget *toolbar);
