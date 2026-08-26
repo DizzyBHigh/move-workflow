62780b8d0370849e39b4ee925cf81e92f2c2e8de
Dizzy B High <dizzybhigh@gmail.com>
2026-08-26 10:03:30 +0200

    Fix Trigger Workflow trigger selector refresh

--- src/workflow-trigger-filter.cpp
+++ src/workflow-trigger-filter.cpp
@@ -56,6 +56,37 @@ static void hide(void *opaque)
         data->visible = false;
 }
 
+static void fill_triggers(obs_property_t *property, const workflow_t *workflow)
+{
+    obs_property_list_clear(property);
+    if (!workflow) {
+        obs_property_list_add_string(property, "Select a workflow first", "");
+        return;
+    }
+
+    bool found = false;
+    for (size_t i = 0; i < workflow->node_count; ++i) {
+        const workflow_node_t &node = workflow->nodes[i];
+        if (node.type != WORKFLOW_NODE_TRIGGER)
+            continue;
+        obs_property_list_add_string(property, node.name, node.id);
+        found = true;
+    }
+
+    if (!found)
+        obs_property_list_add_string(property, "No triggers in this workflow", "");
+}
+
+static bool workflow_modified(obs_properties_t *props, obs_property_t *, obs_data_t *settings)
+{
+    workflow_manager_t *manager = workflow_persistence_manager();
+    workflow_t *workflow = manager
+        ? workflow_manager_find(manager, obs_data_get_string(settings, "workflow"))
+        : nullptr;
+    fill_triggers(obs_properties_get(props, "trigger"), workflow);
+    return true;
+}
+
 static obs_properties_t *properties(void *)
 {
     obs_properties_t *props = obs_properties_create();
@@ -64,6 +95,7 @@ static obs_properties_t *properties(void *)
     obs_property_t *trigger = obs_properties_add_list(
         props, "trigger", "Trigger", OBS_COMBO_TYPE_LIST, OBS_COMBO_FORMAT_STRING);
     obs_property_list_add_string(trigger, "Select a workflow first", "");
+    obs_property_set_modified_callback(workflow, workflow_modified);
 
     workflow_manager_t *manager = workflow_persistence_manager();
     if (manager) {
