902ef1ad05fb5da3c3e2756e78eb41e0006a80b1
Dizzy B High <dizzybhigh@gmail.com>
2026-08-26 13:30:13 +0200

    Fix C/C++ editor header compatibility

--- src/workflow-editor-window.h
+++ src/workflow-editor-window.h
@@ -1,13 +1,13 @@
 #pragma once
 
-class QWidget;
-
-void show_move_workflow_editor(QWidget *parent = nullptr);
-void destroy_move_workflow_editor(void);
-
 #ifdef __cplusplus
+class QWidget;
+void show_move_workflow_editor(QWidget *parent = nullptr);
 extern "C" {
+#else
+void show_move_workflow_editor(void *parent);
 #endif
+void destroy_move_workflow_editor(void);
 void workflow_editor_redo_from_hotkey(void);
 #ifdef __cplusplus
 }
