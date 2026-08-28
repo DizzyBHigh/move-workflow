c713fb98cd8948c538030886640659d2c54f3c68
Dizzy B High <dizzybhigh@gmail.com>
2026-08-28 16:53:26 +0200

    Add global workflow node identity API

---
+++ src/workflow-node-identity.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "workflow-model.h"
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void workflow_node_identity_generate(char *id, size_t size);
+bool workflow_manager_node_ids_unique(const workflow_manager_t *manager);
+void workflow_manager_repair_node_ids(workflow_manager_t *manager);
+bool workflow_node_belongs_to_workflow(const workflow_t *workflow, const char *node_id);
+
+#ifdef __cplusplus
+}
+#endif
