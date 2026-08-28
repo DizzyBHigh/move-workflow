c1986d990aa2daca8310f7b6aeff9f844364bcc6
Dizzy B High <dizzybhigh@gmail.com>
2026-08-28 17:05:59 +0200

    Add panel UI implementation module

---
+++ src/move-workflow-editor-panel-ui.cpp
@@ -0,0 +1,12 @@
+#include "move-workflow-editor-panel-ui.hpp"
+
+#include <QDialog>
+
+namespace move_workflow_editor_panel_ui {
+
+void install(QDialog *dialog)
+{
+    (void)dialog;
+}
+
+} // namespace move_workflow_editor_panel_ui
