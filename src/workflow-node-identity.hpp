062b85654188c4999ebf4e82d900d12d31f704e8
Dizzy B High <dizzybhigh@gmail.com>
2026-08-28 17:46:09 +0200

    Add global workflow node ID allocator

--- src/workflow-node-identity.hpp
+++ src/workflow-node-identity.hpp
@@ -3,12 +3,15 @@
 #include "workflow-model.h"
 
 #include <stdbool.h>
+#include <stddef.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 void workflow_node_identity_generate(char *id, size_t size);
+bool workflow_manager_generate_node_id(const workflow_manager_t *manager,
+                                        char *id, size_t size);
 bool workflow_manager_node_ids_unique(const workflow_manager_t *manager);
 void workflow_manager_repair_node_ids(workflow_manager_t *manager);
 bool workflow_node_belongs_to_workflow(const workflow_t *workflow, const char *node_id);
