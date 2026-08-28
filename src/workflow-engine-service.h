1c2386c78163a511c1aaeef0eeea064b3b1d2383
Dizzy B High <dizzybhigh@gmail.com>
2026-08-28 20:03:13 +0200

    Expose node runtime status to editor

--- src/workflow-engine-service.h
+++ src/workflow-engine-service.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "workflow-engine-node-runtime.h"
 #include <stdbool.h>
 #ifdef __cplusplus
 extern "C" {
@@ -9,6 +10,8 @@ bool workflow_engine_service_test_node(const char *workflow_id, const char *node
 bool workflow_engine_service_trigger(const char *workflow_id, const char *trigger_id);
 bool workflow_engine_service_trigger_scene(const char *scene_name);
 bool workflow_engine_service_workflow_running(const char *workflow_id);
+bool workflow_engine_service_node_runtime(const char *workflow_id, const char *node_id,
+                                          workflow_engine_node_runtime_t *out);
 #ifdef __cplusplus
 }
 #endif
