da12e43f06f4069c3d9a5e9e2a6633ff1020105f
Dizzy B High <dizzybhigh@gmail.com>
2026-08-28 17:31:06 +0200

    Fix obs_source_t declaration in filter settings header

--- src/workflow-filter-settings.h
+++ src/workflow-filter-settings.h
@@ -1,10 +1,9 @@
 #pragma once
 
 #include <cstdint>
+#include <obs.h>
 
 #include "workflow-model.h"
-
-struct obs_source;
 
 void workflow_filter_apply_node_settings(obs_source_t *filter,
                                          const workflow_node_t *node,
