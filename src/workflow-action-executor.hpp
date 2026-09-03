c84c3eee8c9fc52c06b79e8de195902b296e365f
Dizzy B High <dizzybhigh@gmail.com>
2026-09-03 16:10:00 +0200

    Pass workflow run state to action executor

--- src/workflow-action-executor.hpp
+++ src/workflow-action-executor.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
+#include "workflow-engine-state.h"
 #include "workflow-model.h"
 
-bool workflow_action_executor_execute(workflow_t *workflow,
+bool workflow_action_executor_execute(workflow_engine_state_t *state,
                                       workflow_node_t *node);
