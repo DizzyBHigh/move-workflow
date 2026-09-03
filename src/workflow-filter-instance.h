480cf5c068f836778ce5e4b1e4049ee4b1db0507
Dizzy B High <dizzybhigh@gmail.com>
2026-09-03 16:07:23 +0200

    Define workflow runtime filter collection interface

--- src/workflow-filter-instance.h
+++ src/workflow-filter-instance.h
@@ -3,6 +3,13 @@
 #include <obs.h>
 #include "workflow-model.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct workflow_filter_instance workflow_filter_instance;
+typedef struct workflow_filter_instance_set workflow_filter_instance_set;
+
 struct workflow_filter_instance {
 	obs_source_t *original;
 	obs_source_t *instance;
@@ -18,3 +25,15 @@ void workflow_filter_instance_destroy(workflow_filter_instance *instance);
 
 bool workflow_filter_instance_execute_node(workflow_t *workflow,
 										workflow_node_t *node);
+
+workflow_filter_instance_set *workflow_filter_instance_set_create(workflow_t *workflow);
+void workflow_filter_instance_set_destroy(workflow_filter_instance_set *set);
+bool workflow_filter_instance_set_prepare_node(workflow_filter_instance_set *set,
+										workflow_node_t *node);
+workflow_filter_instance *workflow_filter_instance_set_get(
+										workflow_filter_instance_set *set,
+										const workflow_node_t *node);
+
+#ifdef __cplusplus
+}
+#endif
