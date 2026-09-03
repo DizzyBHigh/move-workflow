06bb323b290a604ffba30239f9023bd9d3c3eab7
Dizzy B High <dizzybhigh@gmail.com>
2026-09-03 16:09:40 +0200

    Expose per-run runtime filter ownership

--- src/workflow-engine-runs.h
+++ src/workflow-engine-runs.h
@@ -8,6 +8,7 @@ extern "C" {
 #endif
 
 typedef struct workflow_engine_runs workflow_engine_runs_t;
+typedef struct workflow_filter_instance_set workflow_filter_instance_set;
 
 workflow_engine_runs_t *workflow_engine_runs_create(void);
 void workflow_engine_runs_destroy(workflow_engine_runs_t *runs);
@@ -17,6 +18,8 @@ const workflow_engine_state_t *workflow_engine_run_state_const(const workflow_en
 workflow_engine_run_t *workflow_engine_run_next(const workflow_engine_run_t *run);
 workflow_engine_run_t *workflow_engine_runs_head(workflow_engine_runs_t *runs);
 workflow_engine_run_t *workflow_engine_runs_current(workflow_engine_runs_t *runs);
+workflow_filter_instance_set *workflow_engine_run_filter_instances(workflow_engine_run_t *run);
+void workflow_engine_run_filter_instances_cleanup(workflow_engine_run_t *run);
 void workflow_engine_runs_stop_all(workflow_engine_runs_t *runs);
 bool workflow_engine_runs_any_active(const workflow_engine_runs_t *runs);
 void workflow_engine_run_retain(workflow_engine_run_t *run);
