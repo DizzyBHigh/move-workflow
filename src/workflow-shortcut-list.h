c248af8d1b2f448ee5d24a721e6a6469e3c40074
Dizzy B High <dizzybhigh@gmail.com>
2026-09-05 13:55:45 +0200

    Add workflow shortcut list header

---
+++ src/workflow-shortcut-list.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "workflow-node.h"
+
+#include <QList>
+#include <QString>
+#include <QVector>
+
+#include <cstddef>
+#include <cstdint>
+
+class QLineEdit;
+class QVBoxLayout;
+class WorkflowShortcutKeyEdit;
+
+class WorkflowShortcutList final : public QWidget {
+public:
+    WorkflowShortcutList(const QString &title,
+                         const QString &hint,
+                         NodeItem *current,
+                         const QList<NodeItem *> &nodes,
+                         const char ids[][WORKFLOW_MAX_NAME],
+                         const uint32_t keys[],
+                         const uint32_t modifiers[],
+                         size_t count,
+                         QWidget *parent = nullptr);
+
+    void apply(size_t &count,
+               char ids[][WORKFLOW_MAX_NAME],
+               uint32_t keys[],
+               uint32_t modifiers[]) const;
+
+private:
+    struct ShortcutRow {
+        QString id;
+        WorkflowShortcutKeyEdit *key = nullptr;
+    };
+
+    void rebuildAttachedList();
+    void addAction();
+    void removeAction(const QString &id);
+
+    NodeItem *current_ = nullptr;
+    QList<NodeItem *> nodes_;
+    QVector<ShortcutRow> rows_;
+    QLineEdit *search_ = nullptr;
+    QVBoxLayout *attachedLayout_ = nullptr;
+};
