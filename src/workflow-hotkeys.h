7ffc2a2d826c645b0921809bf33008ed9083c20f
Dizzy B High <dizzybhigh@gmail.com>
2026-09-05 13:41:55 +0200

    Expose shortcut hotkey refresh

--- src/workflow-hotkeys.h
+++ src/workflow-hotkeys.h
@@ -7,6 +7,7 @@ extern "C" {
 typedef void (*workflow_redo_callback_t)(void);
 
 void workflow_hotkeys_register(void);
+void workflow_hotkeys_refresh(void);
 void workflow_hotkeys_unregister(void);
 void workflow_hotkeys_set_redo_callback(workflow_redo_callback_t callback);
 
