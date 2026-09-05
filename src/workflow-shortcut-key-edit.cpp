2262f95013de8ec04ac70c15e3022db6dd8d230c
Dizzy B High <dizzybhigh@gmail.com>
2026-09-05 13:57:35 +0200

    Fix OBS shortcut display conversion

--- src/workflow-shortcut-key-edit.cpp
+++ src/workflow-shortcut-key-edit.cpp
@@ -4,49 +4,48 @@
 #include <QKeyEvent>
 #include <QMouseEvent>
 #include <QPainter>
+#include <QStringList>
 
 namespace {
 uint32_t qtModifiers(Qt::KeyboardModifiers modifiers)
 {
     uint32_t result = 0;
-    if (modifiers & Qt::ShiftModifier)
-        result |= INTERACT_SHIFT_KEY;
-    if (modifiers & Qt::ControlModifier)
-        result |= INTERACT_CONTROL_KEY;
-    if (modifiers & Qt::AltModifier)
-        result |= INTERACT_ALT_KEY;
-    if (modifiers & Qt::MetaModifier)
-        result |= INTERACT_COMMAND_KEY;
+    if (modifiers & Qt::ShiftModifier) result |= INTERACT_SHIFT_KEY;
+    if (modifiers & Qt::ControlModifier) result |= INTERACT_CONTROL_KEY;
+    if (modifiers & Qt::AltModifier) result |= INTERACT_ALT_KEY;
+    if (modifiers & Qt::MetaModifier) result |= INTERACT_COMMAND_KEY;
     return result;
 }
 
 bool isModifierKey(obs_key_t key)
 {
-    switch (key) {
-    case OBS_KEY_SHIFT:
-    case OBS_KEY_CONTROL:
-    case OBS_KEY_ALT:
-    case OBS_KEY_META:
-        return true;
-    default:
-        return false;
+    return key == OBS_KEY_SHIFT || key == OBS_KEY_CONTROL ||
+           key == OBS_KEY_ALT || key == OBS_KEY_META;
+}
+
+QString combinationText(obs_key_combination_t combo)
+{
+    QStringList parts;
+    if (combo.modifiers & INTERACT_CONTROL_KEY) parts << "Ctrl";
+    if (combo.modifiers & INTERACT_COMMAND_KEY) parts << "Meta";
+    if (combo.modifiers & INTERACT_ALT_KEY) parts << "Alt";
+    if (combo.modifiers & INTERACT_SHIFT_KEY) parts << "Shift";
+    if (combo.key != OBS_KEY_NONE) {
+        const char *name = obs_key_to_name(combo.key);
+        if (name && *name) parts << QString::fromUtf8(name);
     }
+    return parts.join(" + ");
 }
 }
 
-WorkflowShortcutKeyEdit::WorkflowShortcutKeyEdit(QWidget *parent)
-    : QWidget(parent)
+WorkflowShortcutKeyEdit::WorkflowShortcutKeyEdit(QWidget *parent) : QWidget(parent)
 {
     setFocusPolicy(Qt::StrongFocus);
     setMinimumHeight(28);
-    setAttribute(Qt::WA_OpaquePaintEvent, false);
     updateDisplay();
 }
 
-obs_key_combination_t WorkflowShortcutKeyEdit::combination() const
-{
-    return combination_;
-}
+obs_key_combination_t WorkflowShortcutKeyEdit::combination() const { return combination_; }
 
 void WorkflowShortcutKeyEdit::setCombination(obs_key_combination_t combination)
 {
@@ -62,13 +61,11 @@ void WorkflowShortcutKeyEdit::keyPressEvent(QKeyEvent *event)
         event->accept();
         return;
     }
-
     const obs_key_t key = obs_key_from_virtual_key(event->nativeVirtualKey());
     if (key == OBS_KEY_NONE || isModifierKey(key)) {
         event->accept();
         return;
     }
-
     combination_.modifiers = qtModifiers(event->modifiers());
     combination_.key = key;
     capturing_ = false;
@@ -77,10 +74,7 @@ void WorkflowShortcutKeyEdit::keyPressEvent(QKeyEvent *event)
     event->accept();
 }
 
-void WorkflowShortcutKeyEdit::keyReleaseEvent(QKeyEvent *event)
-{
-    event->accept();
-}
+void WorkflowShortcutKeyEdit::keyReleaseEvent(QKeyEvent *event) { event->accept(); }
 
 void WorkflowShortcutKeyEdit::focusOutEvent(QFocusEvent *event)
 {
@@ -104,17 +98,10 @@ void WorkflowShortcutKeyEdit::mousePressEvent(QMouseEvent *event)
 void WorkflowShortcutKeyEdit::paintEvent(QPaintEvent *)
 {
     QPainter painter(this);
-    painter.setRenderHint(QPainter::Antialiasing);
     painter.drawRoundedRect(rect().adjusted(0, 0, -1, -1), 3, 3);
-
-    const QString text = capturing_ ? tr("Press shortcut…") :
-                                     (combination_.key == OBS_KEY_NONE ?
-                                          tr("Click and press shortcut…") :
-                                          QString::fromUtf8(obs_key_combination_to_str(combination_)));
+    QString text = capturing_ ? tr("Press shortcut…") : combinationText(combination_);
+    if (text.isEmpty()) text = tr("Click and press shortcut…");
     painter.drawText(rect().adjusted(8, 0, -8, 0), Qt::AlignVCenter, text);
 }
 
-void WorkflowShortcutKeyEdit::updateDisplay()
-{
-    update();
-}
+void WorkflowShortcutKeyEdit::updateDisplay() { update(); }
