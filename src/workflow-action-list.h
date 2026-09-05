faa0364ed68e24f3c736c8c46fefc2a4d80e9fb4
Dizzy B High <dizzybhigh@gmail.com>
2026-09-05 20:35:35 +0200

    Support per-action shortcut editors

--- src/workflow-action-list.h
+++ src/workflow-action-list.h
@@ -7,6 +7,10 @@
 #include <QStringList>
 #include <QWidget>
 
+class NodeItem;
+class QVBoxLayout;
+class QKeySequenceEdit;
+
 class WorkflowActionList final : public QWidget {
 public:
     WorkflowActionList(const QString &title,
@@ -15,7 +19,8 @@ public:
                        const QList<NodeItem *> &nodes,
                        const char ids[][WORKFLOW_MAX_NAME],
                        size_t count,
-                       QWidget *parent = nullptr);
+                       QWidget *parent = nullptr,
+                       bool shortcut_mode = false);
 
     void apply(size_t &count, char ids[][WORKFLOW_MAX_NAME]) const;
 
@@ -23,10 +28,13 @@ private:
     void rebuildAttachedList();
     void addAction();
     void removeAction(const QString &id);
+    void applyShortcutKeys() const;
 
     NodeItem *current_ = nullptr;
     QList<NodeItem *> nodes_;
     QStringList attachedIds_;
+    QList<QKeySequenceEdit *> shortcutEditors_;
     class QLineEdit *search_ = nullptr;
     class QVBoxLayout *attachedLayout_ = nullptr;
+    bool shortcutMode_ = false;
 };
