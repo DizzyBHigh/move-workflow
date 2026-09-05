1eccd6a0e82c23521ef7051606e7ab8280ecbdbd
Dizzy B High <dizzybhigh@gmail.com>
2026-09-05 19:53:13 +0200

    Expose shortcut editor binding state

--- src/workflow-shortcut-settings.h
+++ src/workflow-shortcut-settings.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "workflow-model.h"
-
 #include <QList>
 #include <QString>
 
@@ -20,6 +19,7 @@ QWidget *create_editor(const workflow_node_t *source,
                        const QList<NodeItem *> &nodes,
                        QWidget *parent = nullptr);
 
+bool read(const QWidget *editor, Binding &binding);
 bool apply(const Binding &binding, workflow_node_t *source);
 
 } // namespace workflow_shortcut_settings
