d4aaac4c300b2ae5c737965e7e12998ba2b020e7
Dizzy B High <dizzybhigh@gmail.com>
2026-09-06 15:15:38 +0200

    Resolve workflow sources by OBS UUID directly

--- src/workflow-source-lookup.cpp
+++ src/workflow-source-lookup.cpp
@@ -4,33 +4,12 @@
 
 #include <cstring>
 
-struct source_lookup_context {
-    const char *uuid;
-    obs_source_t *source;
-};
-
-static bool find_source_callback(void *data, obs_source_t *source)
-{
-    auto *context = (source_lookup_context *)data;
-    if (!context || context->source || !source || !context->uuid)
-        return true;
-
-    const char *uuid = obs_source_get_uuid(source);
-    if (uuid && !strcmp(uuid, context->uuid)) {
-        context->source = obs_source_get_ref(source);
-        return false;
-    }
-    return true;
-}
-
 obs_source_t *workflow_find_source_by_uuid(const char *uuid)
 {
     if (!uuid || !*uuid)
         return nullptr;
 
-    source_lookup_context context{uuid, nullptr};
-    obs_enum_sources(find_source_callback, &context);
-    return context.source;
+    return obs_get_source_by_uuid(uuid);
 }
 
 struct source_diagnostic_context {
@@ -56,9 +35,9 @@ static bool diagnose_source_callback(void *data, obs_source_t *source)
 void workflow_log_source_uuid_diagnostics(const char *uuid, const char *name)
 {
     source_diagnostic_context context{uuid, name, 0, 0};
-    obs_enum_sources(diagnose_source_callback, &context);
+    obs_enum_all_sources(diagnose_source_callback, &context);
 
-    workflow_debug_log("Source UUID lookup: uuid='%s' name='%s' top_level_sources=%zu name_matches=%zu",
+    workflow_debug_log("Source UUID lookup: uuid='%s' name='%s' all_sources=%zu name_matches=%zu",
                        uuid ? uuid : "", name ? name : "", context.total,
                        context.name_matches);
 }
