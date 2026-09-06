9b0825cf3a9fb7c1a07172c8f78364d1db4e448c
Dizzy B High <dizzybhigh@gmail.com>
2026-09-06 21:33:06 +0200

    Track persistent node on runtime filter instance

--- src/workflow-filter-instance.h
+++ src/workflow-filter-instance.h
@@ -11,24 +11,25 @@ typedef struct workflow_filter_instance workflow_filter_instance;
 typedef struct workflow_filter_instance_set workflow_filter_instance_set;
 
 struct workflow_filter_instance {
-	obs_source_t *original;
-	obs_source_t *instance;
-	obs_source_t *parent;
+    obs_source_t *original;
+    obs_source_t *instance;
+    obs_source_t *parent;
+    const workflow_node_t *node;
 };
 
 workflow_filter_instance *workflow_filter_instance_create(obs_source_t *original,
-										obs_source_t *parent,
-										const workflow_node_t *node);
+                                                           obs_source_t *parent,
+                                                           const workflow_node_t *node);
 bool workflow_filter_instance_execute(workflow_filter_instance *instance);
 void workflow_filter_instance_destroy(workflow_filter_instance *instance);
 
 workflow_filter_instance_set *workflow_filter_instance_set_create(workflow_t *workflow);
 void workflow_filter_instance_set_destroy(workflow_filter_instance_set *set);
 bool workflow_filter_instance_set_prepare_node(workflow_filter_instance_set *set,
-										workflow_node_t *node);
+                                                workflow_node_t *node);
 workflow_filter_instance *workflow_filter_instance_set_get(
-										workflow_filter_instance_set *set,
-										const workflow_node_t *node);
+    workflow_filter_instance_set *set,
+    const workflow_node_t *node);
 
 #ifdef __cplusplus
 }
