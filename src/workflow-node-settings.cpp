8e8120e60565b5e1121288ee753ad2014fc8daae
Dizzy B High <dizzybhigh@gmail.com>
2026-09-06 21:14:44 +0200

    Add relationship diagnostics to node settings

--- src/workflow-node-settings.cpp
+++ src/workflow-node-settings.cpp
@@ -12,6 +12,25 @@
 #include <QSpinBox>
 #include <QVBoxLayout>
 
+namespace {
+static void log_relationships(const workflow_node_t *node)
+{
+    if (!node) return;
+    blog(LOG_INFO, "[Move Workflow] RELATIONSHIPS node=%s next=%zu simultaneous=%zu shortcuts=%zu bindings=%zu",
+         node->id, node->next_node_count, node->simultaneous_node_count,
+         node->shortcut_node_count, node->shortcut_binding_count);
+    for (size_t i = 0; i < node->next_node_count; ++i)
+        blog(LOG_INFO, "[Move Workflow] RELATIONSHIP next[%zu]=%s", i, node->next_node_ids[i]);
+    for (size_t i = 0; i < node->simultaneous_node_count; ++i)
+        blog(LOG_INFO, "[Move Workflow] RELATIONSHIP simultaneous[%zu]=%s", i, node->simultaneous_node_ids[i]);
+    for (size_t i = 0; i < node->shortcut_node_count; ++i)
+        blog(LOG_INFO, "[Move Workflow] RELATIONSHIP shortcut[%zu]=%s", i, node->shortcut_node_ids[i]);
+    for (size_t i = 0; i < node->shortcut_binding_count; ++i)
+        blog(LOG_INFO, "[Move Workflow] RELATIONSHIP binding[%zu]=%s key=%s", i,
+             node->shortcut_bindings[i].target_id, node->shortcut_bindings[i].key);
+}
+}
+
 NodeSettingsDialog::NodeSettingsDialog(NodeItem *node,const QList<NodeItem *> &nodes,QWidget *parent):QDialog(parent),node_(node),nodes_(nodes)
 {
     setWindowTitle(QString("Node Settings - %1").arg(node?node->nodeName():"Node")); resize(560,800); setMinimumSize(500,400);
@@ -30,8 +49,7 @@ bool NodeSettingsDialog::apply()
     else{const QString parentName=source_->currentText().trimmed();const QString filterName=filter_->currentText().trimmed();if(parentName.isEmpty()){wf->action.kind=WORKFLOW_MOVE_ACTION;wf->action.scene_name[0]='\0';wf->action.source_name[0]='\0';wf->action.filter_name[0]='\0';wf->action.filter_id[0]='\0';}else if(filterName.isEmpty()){wf->action.kind=WORKFLOW_MOVE_ACTION;settings_copy_text(wf->action.scene_name,WORKFLOW_MAX_NAME,parentName);wf->action.source_name[0]='\0';wf->action.filter_name[0]='\0';wf->action.filter_id[0]='\0';}else{obs_source_t *parent=obs_get_source_by_name(parentName.toUtf8().constData());if(!parent)return false;obs_source_t *filter=obs_source_get_filter_by_name(parent,filterName.toUtf8().constData());if(!filter){obs_source_release(parent);return false;}const char *filterId=obs_source_get_id(filter);if(!settings_supported_filter(filterId)){obs_source_release(filter);obs_source_release(parent);return false;}wf->action.kind=settings_kind(filterId);settings_copy_text(wf->action.scene_name,WORKFLOW_MAX_NAME,parentName);wf->action.source_name[0]='\0';settings_copy_text(wf->action.filter_name,WORKFLOW_MAX_NAME,filterName);settings_copy_text(wf->action.filter_id,WORKFLOW_MAX_NAME,QString::fromUtf8(filterId));obs_source_release(filter);obs_source_release(parent);}}
     wf->start_delay.mode=startDelayDefault_->isChecked()?WORKFLOW_USE_EXISTING:WORKFLOW_OVERRIDE;wf->start_delay.delay_ms=startDelayDefault_->isChecked()?startDelayOverrideMs_:(uint64_t)startDelayMs_->value();wf->duration.mode=durationDefault_->isChecked()?WORKFLOW_USE_EXISTING:WORKFLOW_OVERRIDE;wf->duration.duration_ms=durationDefault_->isChecked()?durationOverrideMs_:(uint64_t)durationMs_->value();wf->end_delay.mode=endDelayDefault_->isChecked()?WORKFLOW_USE_EXISTING:WORKFLOW_OVERRIDE;wf->end_delay.delay_ms=endDelayDefault_->isChecked()?endDelayOverrideMs_:(uint64_t)endDelayMs_->value();wf->easing.mode=easingDefault_->isChecked()?WORKFLOW_USE_EXISTING:WORKFLOW_OVERRIDE;wf->easing.easing=(workflow_easing_t)easingType_->currentData().toInt();wf->easing.function=(workflow_easing_function_t)easingFunction_->currentData().toInt();
     simultaneous_->apply(wf->simultaneous_node_count,wf->simultaneous_node_ids);nextActions_->apply(wf->next_node_count,wf->next_node_ids);shortcutActions_->apply(wf->shortcut_node_count,wf->shortcut_node_ids);wf->simultaneous_actions_mode=WORKFLOW_OVERRIDE;wf->next_actions_mode=WORKFLOW_OVERRIDE;
-    workflow_hotkeys_refresh();
-    return true;
+    log_relationships(wf); workflow_hotkeys_refresh(); return true;
 }
 
 bool edit_node_settings(NodeItem *node,const QList<NodeItem *> &nodes,QWidget *parent){NodeSettingsDialog dialog(node,nodes,parent);return dialog.exec()==QDialog::Accepted;}
