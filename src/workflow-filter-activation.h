5878b48ca329a476bb612a222add660f1551d600
Dizzy B High <dizzybhigh@gmail.com>
2026-09-06 21:32:43 +0200

    Expose runtime Move filter preparation

--- src/workflow-filter-activation.h
+++ src/workflow-filter-activation.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include "workflow-model.h"
 #include <obs.h>
 
-bool workflow_filter_restore(obs_source_t *runtime, obs_source_t *original);
+bool workflow_filter_prepare_for_execution(obs_source_t *runtime, obs_source_t *original,
+                                           const workflow_node_t *node);
 bool workflow_filter_activate(obs_source_t *source, obs_source_t *parent);
