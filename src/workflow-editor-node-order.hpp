5681b058cea3de542373ab7fcca06b05eec01763
Dizzy B High <dizzybhigh@gmail.com>
2026-09-07 11:35:32 +0200

    Add sidebar node order module interface

---
+++ src/workflow-editor-node-order.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <QList>
+#include <QString>
+#include <QStringList>
+
+class NodeItem;
+
+namespace workflow_editor_node_order {
+
+QStringList ordered_node_ids(const QString &workflowId,
+                             const QList<NodeItem *> &nodes);
+void save_order(const QString &workflowId, const QStringList &nodeIds);
+QStringList merge_order(const QStringList &storedIds,
+                        const QList<NodeItem *> &nodes);
+
+} // namespace workflow_editor_node_order
