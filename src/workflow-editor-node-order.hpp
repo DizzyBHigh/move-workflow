6d5fb8d71ef4faf85a1c962d59a84988577b0fee
Dizzy B High <dizzybhigh@gmail.com>
2026-09-07 11:58:09 +0200

    Add node order persistence accessor

--- src/workflow-editor-node-order.hpp
+++ src/workflow-editor-node-order.hpp
@@ -10,6 +10,7 @@ namespace workflow_editor_node_order {
 
 QStringList ordered_node_ids(const QString &workflowId,
                              const QList<NodeItem *> &nodes);
+QStringList stored_order(const QString &workflowId);
 void save_order(const QString &workflowId, const QStringList &nodeIds);
 QStringList merge_order(const QStringList &storedIds,
                         const QList<NodeItem *> &nodes);
