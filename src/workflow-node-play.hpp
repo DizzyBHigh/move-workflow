4738851cb81cc866a2263282f29d93794a584c28
Dizzy B High <dizzybhigh@gmail.com>
2026-09-07 11:18:28 +0200

    Correct node play helper interface

--- src/workflow-node-play.hpp
+++ src/workflow-node-play.hpp
@@ -3,11 +3,12 @@
 class NodeItem;
 class QPainter;
 class QPointF;
+class QRectF;
 
 namespace workflow_node_play {
 
 void paint(QPainter *painter, const NodeItem *node, const QRectF &rect);
 bool contains(const NodeItem *node, const QPointF &scenePos);
-void execute(const NodeItem *node);
+void execute(const NodeItem *node, const char *workflowId);
 
 } // namespace workflow_node_play
