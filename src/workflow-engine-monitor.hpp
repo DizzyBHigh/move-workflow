7472bd2a189519861d6bec208f731ea154836a24
Dizzy B High <dizzybhigh@gmail.com>
2026-09-07 12:45:57 +0200

    Align monitor API with engine boundary

--- src/workflow-engine-monitor.hpp
+++ src/workflow-engine-monitor.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "workflow-engine-runs.h"
+#include "workflow-engine.h"
 #include <QList>
 #include <QString>
 
@@ -15,10 +15,10 @@ struct RunInfo {
     bool waiting_for_shortcut = false;
 };
 
-QList<RunInfo> active_runs(workflow_engine_runs_t *runs,
+QList<RunInfo> active_runs(workflow_engine_t *engine,
                            const QString &workflow_id = QString());
 
-bool stop_run(workflow_engine_runs_t *runs, uint64_t run_id);
-size_t stop_workflow(workflow_engine_runs_t *runs, const QString &workflow_id);
+bool stop_run(workflow_engine_t *engine, uint64_t run_id);
+size_t stop_workflow(workflow_engine_t *engine, const QString &workflow_id);
 
 }
