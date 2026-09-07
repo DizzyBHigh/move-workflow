96f14932626e42ab43e8814e279e4064e7f65c1e
Dizzy B High <dizzybhigh@gmail.com>
2026-09-07 12:49:13 +0200

    Pass engine to active runs panel

--- src/workflow-editor-active-runs.hpp
+++ src/workflow-editor-active-runs.hpp
@@ -3,10 +3,11 @@
 #include <QString>
 
 class QWidget;
+struct workflow_engine;
 
 namespace workflow_editor_active_runs {
 
-QWidget *create(QWidget *parent = nullptr);
+QWidget *create(QWidget *parent = nullptr, workflow_engine *engine = nullptr);
 void set_workflow(QWidget *panel, const QString &workflowId);
 void refresh(QWidget *panel);
 
