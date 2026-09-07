822dde4fde5e0f4974f74dd593774aa4ee7b5976
Dizzy B High <dizzybhigh@gmail.com>
2026-09-07 11:35:46 +0200

    Expose sidebar workflow ordering state

--- src/workflow-editor-sidebar.h
+++ src/workflow-editor-sidebar.h
@@ -20,4 +20,5 @@ struct workflow_editor_sidebar_callbacks {
 QWidget *create_workflow_editor_sidebar(QWidget *parent,
                                         workflow_editor_sidebar_callbacks callbacks);
 void workflow_editor_sidebar_set_selection_state(QWidget *sidebar, bool has_selection, bool can_paste);
+void workflow_editor_sidebar_set_workflow_id(QWidget *sidebar, const char *workflowId);
 void workflow_editor_sidebar_set_workflow_nodes(QWidget *sidebar, const QList<NodeItem *> &nodes, NodeItem *selected);
